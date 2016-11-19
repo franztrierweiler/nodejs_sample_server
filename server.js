b08e5a4aba517a424aeebbf6670dbdb5d84b784b
System Administrator <root@MacBook-Pro-de-Franz.local>
2016-11-20 00:18:29 +0100

    Added tests on arguments when receiving a request Added comment to debug

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 // Let's require/import the HTTP module
+// For debug (Franz advice)
+// First way: put debugger keyword in the code and launch node debug server.js
+// Second (more comfortable) way: use node-inspector and just node --debug-brk server.js
 var http = require('http');
 
 // Let's define a port we want to listen to
-const PORT=8080; 
+const PORT=8080;
 
 // We need a function which handles requests and send response
 function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
+    if (request.url == '/')
+    {
+    	response.write('Vous êtes au début');
+    }
+    else if (request.url == "/franz")
+    {
+    	response.write('Vous êtes chez Franz\n');
+    }
+    else
+    {
+    	response.write('Vous êtes à un endroit indéterminé');
+    }
+    response.end();
 }
 
 // Create a server
